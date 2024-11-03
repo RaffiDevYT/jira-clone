0f31bd12c665c6d9fe23bf3f8444d91042fef897
Raffi Athallah.1602 <raffidevyt@gmail.com>
2024-11-03 19:31:18 +0700

    standalone page

--- src/app/(standalone)/layout.tsx
+++ src/app/(standalone)/layout.tsx
@@ -1,27 +1,24 @@
+import { UserButton } from "@/features/auth/components/user-button";
 import Image from "next/image";
 import Link from "next/link";
+import { PropsWithChildren } from "react";
 
-interface StandaloneLayoutProps {
-  children: React.ReactNode;
-}
-export default function StandaloneLayout({ children }: StandaloneLayoutProps) {
-  return (
-    <main className="bg-neutral-100 min-h-screen">
-      <div className="mx-auto max-w-screen-2xl h-full p-4">
-        <nav className="flex justify-between items-center h-[73px]">
-          <Link href="/">
-            <Image
-              src={"/task-tracker-01.svg"}
-              width={152}
-              height={56}
-              alt="logo"
-            />
-          </Link>
-        </nav>
-        <div className="flex flex-col items-center justify-center py-4">
-          {children}
-        </div>
-      </div>
-    </main>
-  );
-}
+const StandaloneLayout = ({ children }: PropsWithChildren) => {
+	return (
+		<main className="min-h-screen bg-neutral-100">
+			<div className="mx-auto max-w-screen-2xl p-4">
+				<nav className="flex justify-between items-center h-[73px]">
+					<Link href="/">
+						<Image src="/logo.png" width={152} height={56} alt="logo" />
+					</Link>
+					<UserButton />
+				</nav>
+				<div className="flex flex-col items-center justify-center py-4">
+					{children}
+				</div>
+			</div>
+		</main>
+	);
+};
+
+export default StandaloneLayout;
