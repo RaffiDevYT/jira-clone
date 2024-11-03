a6ee61d7ce5d89146f92c8c6faab0a821602c002
Raffi Athallah.1602 <raffidevyt@gmail.com>
2024-11-03 19:30:44 +0700

    add task page

---
+++ src/app/(dashboard)/workspaces/[workspaceId]/tasks/page.tsx
@@ -0,0 +1,26 @@
+import { getCurrent } from "@/features/auth/queries";
+import { TaskViewSwitcher } from "@/features/tasks/components/task-view-switcher";
+import { redirect } from "next/navigation";
+import type { Metadata } from "next";
+
+// title: "Jira > Tasks"
+export const metadata: Metadata = {
+	title: "Jira > Tasks",
+	description: "View and manage your tasks here",
+	icons: {
+		icon: "/favicon.png",
+	},
+};
+
+const TasksPage = async () => {
+	const current = await getCurrent();
+	if (!current) redirect("/sign-in");
+
+	return (
+		<div className="h-full flex flex-col">
+			<TaskViewSwitcher />
+		</div>
+	);
+};
+
+export default TasksPage;
