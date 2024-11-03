c33024b46e687c46de2cbbd3d85020250559e82e
Raffi Athallah.1602 <raffidevyt@gmail.com>
2024-11-03 19:30:20 +0700

    reafactor server components for only redirecting

---
+++ src/app/(dashboard)/workspaces/[workspaceId]/projects/[projectId]/page.tsx
@@ -0,0 +1,21 @@
+import { redirect } from "next/navigation";
+import { getCurrent } from "@/features/auth/queries";
+import { ProjectIdClient } from "./client";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+	title: "Jira > Projects",
+	description: "View and manage your projects here",
+	icons: {
+		icon: "/favicon.png",
+	},
+};
+
+const ProjectIdPage = async () => {
+	const current = await getCurrent();
+	if (!current) redirect("/sign-in");
+	
+	return <ProjectIdClient />;
+};
+
+export default ProjectIdPage;
