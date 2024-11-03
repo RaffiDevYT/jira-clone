fb951a40327007004f3bdbf8cf14dc6ec7bcc327
Raffi Athallah.1602 <raffidevyt@gmail.com>
2024-11-03 19:36:28 +0700

    add auth pages

--- src/components/ui/form.tsx
+++ src/components/ui/form.tsx
@@ -2,18 +2,11 @@
 
 import * as React from "react"
 import * as LabelPrimitive from "@radix-ui/react-label"
-import { Slot } from "@radix-ui/react-slot"
-import {
-  Controller,
-  ControllerProps,
-  FieldPath,
-  FieldValues,
-  FormProvider,
-  useFormContext,
-} from "react-hook-form"
+import {Slot} from "@radix-ui/react-slot"
+import {Controller, ControllerProps, FieldPath, FieldValues, FormProvider, useFormContext,} from "react-hook-form"
 
-import { cn } from "@/lib/utils"
-import { Label } from "@/components/ui/label"
+import {cn} from "@/lib/utils"
+import {Label} from "@/components/ui/label"
 
 const Form = FormProvider
 
@@ -135,7 +128,7 @@ const FormDescription = React.forwardRef<
     <p
       ref={ref}
       id={formDescriptionId}
-      className={cn("text-sm text-muted-foreground", className)}
+      className={cn("text-[0.8rem] text-muted-foreground", className)}
       {...props}
     />
   )
@@ -157,7 +150,7 @@ const FormMessage = React.forwardRef<
     <p
       ref={ref}
       id={formMessageId}
-      className={cn("text-sm font-medium text-destructive", className)}
+      className={cn("text-[0.8rem] font-medium text-destructive", className)}
       {...props}
     >
       {body}
