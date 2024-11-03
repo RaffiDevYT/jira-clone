618faf0c6bdfe8eee06f9f044fbee7ca850106a4
Raffi Athallah.1602 <raffidevyt@gmail.com>
2024-11-03 19:31:48 +0700

    reafactor server components for only redirecting

--- src/app/(standalone)/workspaces/[workspaceId]/join/[inviteCode]/page.tsx
+++ src/app/(standalone)/workspaces/[workspaceId]/join/[inviteCode]/page.tsx
@@ -1,27 +1,13 @@
 import { redirect } from "next/navigation";
 
 import { getCurrent } from "@/features/auth/queries";
-import { JoinWorkspaceForm } from "@/features/workspaces/components/join-workspace-form";
-import { getWorkspaceInfo } from "@/features/workspaces/queries";
 
-interface JoinWorkspaceProps {
-  params: { workspaceId: string; inviteCode: string };
-}
+import { WorkspaceIdJoinClient } from "./client";
 
-export default async function JoinWorkspacePage({
-  params,
-}: JoinWorkspaceProps) {
-  const { workspaceId } = params;
+const WorkspaceIdJoinPage = async () => {
+	const current = await getCurrent();
+	if (!current) redirect("/sign-in");
+	return <WorkspaceIdJoinClient />;
+};
 
-  const user = await getCurrent();
-  if (!user) redirect("/sign-in");
-
-  const workspace = await getWorkspaceInfo({ workspaceId });
-  if (!workspace) redirect("/");
-
-  return (
-    <div className="w-full lg:max-w-xl">
-      <JoinWorkspaceForm initialValue={workspace} />
-    </div>
-  );
-}
+export default WorkspaceIdJoinPage;
