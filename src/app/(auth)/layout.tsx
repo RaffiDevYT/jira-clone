bbb9649b0e62defd23591c70de5a0b4405a462e3
Raffi Athallah.1602 <raffidevyt@gmail.com>
2024-11-03 19:26:27 +0700

    add auth pages

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,39 +1,30 @@
 "use client";
-
+import { usePathname } from "next/navigation";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import { PropsWithChildren } from "react";
 import Link from "next/link";
-import { usePathname } from "next/navigation";
 
-interface AuthLayoutProps {
-  children: React.ReactNode;
-}
+const AuthLayout = ({ children }: PropsWithChildren) => {
+	const pathname = usePathname();
+	const isSignIn = pathname === "/sign-in";
+	return (
+		<main className="bg-neutral-100 min-h-screen">
+			<div className="mx-auto max-w-screen-2xl p-4">
+				<nav className="flex items-center justify-between">
+					<Image src="/logo.png" width={150} height={56} alt="logo" />
 
-export default function AuthLayout({ children }: AuthLayoutProps) {
-  const pathname = usePathname();
-  const isSignUp = pathname === "/sign-up";
-
-  return (
-    <main className="bg-neutral-100 min-h-screen">
-      <div className="mx-auto max-w-screen-2xl p-4">
-        <nav className="flex items-center justify-between">
-          <Image
-            width={152}
-            height={56}
-            src={"/task-tracker-01.svg"}
-            alt="logo"
-            className="rounded-md border"
-          />
-          <Button asChild variant="secondary">
-            <Link href={isSignUp ? "/sign-in" : "/sign-up"}>
-              {isSignUp ? "Sign In" : "Sign Up"}
-            </Link>
-          </Button>
-        </nav>
-        <div className="flex flex-col items-center justify-center pt-4 md:pt-14">
-          {children}
-        </div>
-      </div>
-    </main>
-  );
-}
+					<Button asChild variant="secondary">
+						<Link href={isSignIn ? "/sign-up" : "/sign-in"}>
+							{isSignIn ? "Sign Up" : "Login"}
+						</Link>
+					</Button>
+				</nav>
+				<div className="flex flex-col items-center justify-center pt-4 md:py-14">
+					{children}
+				</div>
+			</div>
+		</main>
+	);
+};
+export default AuthLayout;
