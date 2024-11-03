de422afd36f92f3dd5b30f4a493f3180677f929d
Raffi Athallah.1602 <raffidevyt@gmail.com>
2024-11-03 19:20:14 +0700

    updated

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,7 @@
 export const DATABASE_ID = process.env.NEXT_PUBLIC_APPWRITE_DATABASE_ID!;
-export const WORKSPACES_ID = process.env.NEXT_PUBLIC_APPWRITE_WORKSPACES_ID!; //collection id
-export const MEMBERS_ID = process.env.NEXT_PUBLIC_APPWRITE_MEMBERS_ID!; //collection id
-export const PROJECTS_ID = process.env.NEXT_PUBLIC_APPWRITE_PROJECTS_ID!; //collection id
+export const WORKSPACE_ID = process.env.NEXT_PUBLIC_APPWRITE_WORKSPACES_ID!;
+export const MEMBERS_ID = process.env.NEXT_PUBLIC_APPWRITE_MEMBERS_ID!;
+export const PROJECTS_ID = process.env.NEXT_PUBLIC_APPWRITE_PROJECTS_ID!;
+export const TASKS_ID = process.env.NEXT_PUBLIC_APPWRITE_TASKS_ID!;
 export const IMAGES_BUCKET_ID =
-  process.env.NEXT_PUBLIC_APPWRITE_IMAGES_BUCKET_ID!; //bucket storage id
+	process.env.NEXT_PUBLIC_APPWRITE_IMAGES_BUCKET_ID!;
