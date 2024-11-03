d6bd78d9774210053449b0a714600d032b7e136a
Raffi Athallah.1602 <raffidevyt@gmail.com>
2024-11-03 19:26:50 +0700

    add create-workspace form

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,12 +1,12 @@
 import { redirect } from "next/navigation";
 
-import { getCurrent } from "@/features/auth/queries";
 import { SignInCard } from "@/features/auth/components/sign-in-card";
+import { getCurrent } from "@/features/auth/queries";
 
-export default async function SignInPage() {
-  const user = await getCurrent();
+const SignIn = async () => {
+	const user = await getCurrent();
+	if (user) redirect("/");
 
-  if (user) redirect("/");
-
-  return <SignInCard />;
-}
+	return <SignInCard />;
+};
+export default SignIn;
